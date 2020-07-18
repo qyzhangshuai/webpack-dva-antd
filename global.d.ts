7f7a5e05fbc5a9bef45762dd818af1ca308bb268
zhangshuai <zhangshuai08@sunlands.com>
2020-07-18 14:33:41 +0800

    修改

--- global.d.ts
+++ global.d.ts
@@ -2,63 +2,24 @@
  * @description: 全局配置
  * @author: zs
  * @Date: 2020-06-11 16:43:01
- * @LastEditTime: 2020-07-18 11:29:03
+ * @LastEditTime: 2020-07-18 14:32:09
  * @LastEditors: zs
  */
-declare module '*.css' {
-  const content: {
-    [propName: string]: any
-  };
-  export default content;
-}
+declare module '*.css'
+declare module '*.scss'
+declare module '*.less'
+declare module '*.json'
+declare module '*.jpg'
+declare module '*.jpge'
+declare module '*.png'
+declare module '*.gif'
+declare module '*.svg'
+declare module '*.bmp'
 
-declare module '*.scss' {
-  const content: any;
-  export default content;
-}
-
-declare module '*.less' {
-  const content: any;
-  export default content;
-}
-
-declare module '*.json' {
-  const content: any;
-  export default content;
-}
-declare module '*.jpg' {
-  const content: any;
-  export default content;
-}
-declare module '*.jpge' {
-  const content: any;
-  export default content;
-}
-declare module '*.png' {
-  const content: any;
-  export default content;
-}
-declare module '*.gif' {
-  const content: any;
-  export default content;
-}
-declare module '*.svg' {
-  const content: any;
-  export default content;
-}
-declare module '*.bmp' {
-  const content: any;
-  export default content;
-}
+declare module 'dva-loading'
+declare module 'dva-model-extend'
 
 interface Window {
   __state: any
   __app: any
 }
-
-interface NodeModule {
-  hot: any
-}
-
-declare module 'dva-loading'
-declare module 'dva-model-extend'
